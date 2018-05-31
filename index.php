f8710c86543c56dd21e632e2300703eeb2147778
JakeSparkleChicken <37343871+JakeSparkleChicken@users.noreply.github.com>
2018-05-30 19:00:27 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<style>
+label{display:inline-block;width:100px;margin-bottom:10px;}
+</style>
+
+<title>Pick Lamp Color</title>
+<body>
+<form method="post" action="color.php">
+<label>Color</label>
+<input type="color" name="color" />
+<br />
+<input type="submit" value="Add Data">
+</form>
+</body>
+</html>
